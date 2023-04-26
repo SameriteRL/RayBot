86109c57a48210431084793947eb5d00d980442f
SameriteRL <raymondchen733@gmail.com>
2023-04-26 14:42:43 -0400

    Updated load_cogs()
    Function now only displays successfully loaded cogs instead of all modules that were read, and outputs errors. Also changed Bot.loaded_cogs into a set.

--- src/bot.py
+++ src/bot.py
@@ -6,6 +6,9 @@ from globals import *
 
 # Must take in a directory (folder), path cannot be a simple file
 async def load_cogs(path:str, bot:commands.Bot):
+    if not os.path.isdir(path):
+        print(f"\n{path} is not a directory! Aborting cog loading.")
+        return
     for file_name in os.listdir(path):
         new_path = os.path.join(path, file_name)
         if os.path.isdir(new_path):
@@ -13,32 +16,35 @@ async def load_cogs(path:str, bot:commands.Bot):
         else:
             if file_name.endswith(".py"):
                 cog = new_path[2:-3].replace("\\", ".")
-                bot.loaded_cogs.append(cog)
                 try: await bot.load_extension(cog)
-                except: pass
+                except commands.NoEntryPointError:
+                    print(f"\n{cog} has no 'setup' function, ignoring file.")
+                except Exception as err:
+                    print(f"\n{err}")
+                else: bot.loaded_cogs.add(cog)
 
 class Bot(commands.Bot):
 
     def __init__(self, prefix:str, intents:discord.Intents):
         super().__init__(command_prefix=prefix, intents=intents)
         self.token = open("TOKEN.txt").read().strip()
-        self.loaded_cogs = []
+        self.loaded_cogs = set()
 
     async def setup_hook(self):
         await load_cogs(".\src\cogs", self)
     
     async def on_ready(self):
-        python_ver = ".".join(str(e) for e in sys.version_info[:3])
+        python_ver = ".".join(str(ver) for ver in sys.version_info[:3])
         num_synced = len(await self.tree.sync())
-        print("==================================")
+        print("\n==================================")
         print(">>" + f"Logged in as {self.user.name}#{self.user.discriminator}".center(30) + "<<")
         print(">>" + f"Discord.py version {discord.__version__}".center(30) + "<<")
         print(">>" + f"Python version {python_ver}".center(30) + "<<")
         print(f"{num_synced} command(s) synced!".center(34))
         print("==================================\n")
-        print(f"{len(self.loaded_cogs)} module(s) successfully loaded:")
-        for cog in self.loaded_cogs: print(f"- {cog}")
+        print(f"Loaded {len(self.loaded_cogs)} extension(s):")
+        for cog in self.loaded_cogs: print(f" - {cog}")
         print("\n==================================\n")
         print(f"Deployed in {len(self.guilds)} guild(s):")
         for guild in self.guilds: print(f" - {guild.name}")
-        print("\n==================================")
+        print("\n==================================\n")
